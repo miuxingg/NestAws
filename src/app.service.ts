8a4ad569dad91f41e07f6ddb4d86e05937dd9002
Nguyen Thanh Binh <mtt@Kemmm-2.local>
2025-06-17 10:55:26 +0700

    init

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! This is my first nestjs app! I study github actions now.';
+    return 'Hello World! This is my first nestjs app! I study github actions now!';
   }
 }
